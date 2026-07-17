36d454544c7f1c198ba8616e3dae3179368b61ea
AAMQ <h8myjt2785@privaterelay.appleid.com>
2026-07-17 10:21:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function startApp() {
-    alert("Hello Coffee. ☕");
+
+    let name = document.getElementById("nameInput").value;
+
+    if (name === "") {
+
+        alert("Write your name first ☕");
+
+        return;
+
+    }
+
+
+    alert("Hello, " + name + " 👋");
+
 }
